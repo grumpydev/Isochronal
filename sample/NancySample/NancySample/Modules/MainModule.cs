05eaa358ab01fdcf00fb36fe6b2ac7c6b4f19e95
Steven Robbins <ste.robbins@gmail.com>
2011-11-18 15:59:15 +0000

    Updated samples

---
+++ sample/NancySample/NancySample/Modules/MainModule.cs
@@ -0,0 +1,16 @@
+﻿namespace NancySample.Modules
+{
+    using System;
+
+    using Nancy;
+
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = _ => View["index"];
+
+            Get["/clock"] = _ => DateTime.Now.ToShortTimeString();
+        }
+    }
+}
---
+++ sample/NancySample/NancySample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NancySample
+{
+    using System.Diagnostics;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:8888/Nancy/"));
+            host.Start();
+            Process.Start("http://localhost:8888/Nancy/");
+            Console.WriteLine("Press [ENTER] to stop");
+            Console.ReadLine();
+            host.Stop();
+        }
+    }
+}
